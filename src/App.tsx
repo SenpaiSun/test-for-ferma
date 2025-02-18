6eec3aaeb14b152c6f73f456d917bc4c90fc75a4
SenpaiSun <www.maxxi@mail.ru>
2025-02-18 20:47:28 +0600

    feat: add card task

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Title } from './ui/Title/Title';
-import { InputTask } from './components/InputTask/InputTask';
-import { Filters } from './components/InputTask/Filters/Filters';
+import React from 'react'
+import styled from 'styled-components'
+import { Title } from './ui/Title/Title'
+import { InputTask } from './components/InputTask/InputTask'
+import { Filters } from './components/InputTask/Filters/Filters'
+import { Tasks } from './components/InputTask/Tasks/Tasks'
 
 const StyledMain = styled.main`
   display: flex;
@@ -10,18 +11,28 @@ const StyledMain = styled.main`
   align-items: center;
   justify-content: center;
   margin: 100px;
-  wrap: no-wrap;
 `
+const StyledContainerDiv = styled.div`
+  width: 100%;
+  max-width: 1240px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+
 
 const App: React.FC = () => {
   return (
     <StyledMain>
-      <Title text='Список дел'/>
-      <InputTask />
-      <Filters />
+      <StyledContainerDiv>
+        <Title text='Список дел' />
+        <InputTask />
+        <Filters />
+        <Tasks />
+      </StyledContainerDiv>
     </StyledMain>
-  );
-};
-
+  )
+}
 
 export default App
--- src/components/InputTask/Tasks/Tasks.tsx
+++ src/components/InputTask/Tasks/Tasks.tsx
@@ -1,5 +1,17 @@
 import React from 'react'
+import { CardTask } from '../../../ui/CardTask/CardTask'
+import styled from 'styled-components'
+
+const StyledTaskDiv = styled.div`
+  margin-top: 31px;
+  width: 100%;
+  gap: 20px;
+`
 
 export const Tasks: React.FC = () => {
-  return <div></div>
+  return (
+    <StyledTaskDiv>
+      <CardTask />
+    </StyledTaskDiv>
+  )
 }
